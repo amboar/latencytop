64b4afe57c4c1a5bcb7b5c5c56e455da0a67fe14
Andrew Jeffery <andrew@aj.id.au>
2017-09-13 13:14:17 +0930

    fsync: Fix a bunch of warnings
    Missing includes lead to a missing prototype for gettimeofday(), other
    static function definitions were missing void return types and so
    issued a warning about defaulting to int.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- src/fsync.c
+++ src/fsync.c
@@ -32,6 +32,7 @@
 #include <time.h>
 #include <wchar.h>
 #include <ctype.h>
+#include <sys/time.h>
 
 #include <glib.h>
 
@@ -51,7 +52,7 @@ struct fsync_files {
 static GList *fsync_data;
 
 
-static chain_file(struct fsync_process *proc, char *filename)
+static void chain_file(struct fsync_process *proc, char *filename)
 {
 	struct fsync_files *file;
 	GList *item;
@@ -75,7 +76,7 @@ static chain_file(struct fsync_process *proc, char *filename)
 	proc->files = g_list_append(proc->files, file);
 }
 
-static report_file(char *process, char *file)
+static void report_file(char *process, char *file)
 {
 	struct fsync_process *proc;
 	GList *item;
